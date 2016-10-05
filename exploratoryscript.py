b7c621d24035d3eafde19ad32c9154eb7199792b
Antoine Gagne <antoinegturcotte@gmail.com>
2016-10-05 10:28:13 -0400

    add latex template, analysis.txt contains some direction ideas for project

--- exploratoryscript.py
+++ exploratoryscript.py
@@ -3,25 +3,30 @@ import pandas, numpy
 import matplotlib.pyplot as plt
 import ipdb
 
+# Opens the csv and break using ipdb so that it can be explored
+
 filename = "travistorrent-5-3-2016.csv"
-
-
-def plot_histo_nominal(data):
-	data.value_counts().plot(kind='bar')
-	plt.show()
-
-def plot_histo(data):
-	data.hist()
-	plt.show()
-
 
 df = pandas.read_csv(filename)
 
 ipdb.set_trace()
 
-
-
 """
+
+# Examples
+
+# time spent vs time lost vs time wasted
+sub = df[["git_branch","gh_project_name","gh_is_pr","tr_status","git_merged_with"]]
+
+# Complexity of a problem vs chance for it to get integrated
+sub = df[["gh_num_issue_comments","gh_num_pr_comments", "tr_status"]]
+## Only those with at least a comment
+subsub = sub[sub["gh_num_pr_comments"]>0]
+
+# failures per loc : Complexity
+sub = df[["gh_project_name","gh_sloc","tr_status"]]
+x = [sub[sub["gh_project_name"]==x] for x in ["rails/rails", "chef/chef", "jruby/jruby", "rapid7/metasploit-framework"]]
+
 # histogram of nominal data
 plot_histo_nominal(df.tr_status)
 
@@ -38,5 +43,7 @@ data.gh_team_size.hist()
 sub = df[["gh_project_name","gh_sloc","git_num_committers"]]
 data = sub.groupby("gh_project_name").agg(['max'])
 plt.scatter(data["git_num_committers"],data["gh_sloc"])
+
+sub = df[["gh_project_name","gh_test_lines_per_kloc", "gh_sloc"]]
 
 """
