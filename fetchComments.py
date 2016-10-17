1b36eb5a497fe797c1d6be6a38958dfb9c49cc90
olgrea <olgrea@gmail.com>
2016-10-16 22:28:56 -0400

    Added fallback mechanism to handle errors (dump partial file)

--- fetchComments.py
+++ fetchComments.py
@@ -9,6 +9,7 @@ import util
 import json
 import pandas
 import sys
+import os
 from pprint import pprint
 
 #initial files
@@ -20,6 +21,7 @@ travisData = "travistorrent_30_9_2016.csv"
 
 # Output
 outputfile = "./data/comments.csv"
+partialOutputfile = "./data/comments_partial.csv"
 
 def get_from_config(section, config_tags):
 	# Reads the configFile file and returns the config tags located in specified section.
@@ -67,6 +69,7 @@ def build_comment_data(cData, job, commitId, cType, reactions=""):
 	#ipdb.set_trace()
 	data = [
 		job.row,
+		job.gh_project_name,
 		job.git_commit,
 		commitId,
 		job.gh_pull_req_num,
@@ -93,12 +96,13 @@ if __name__ == "__main__":
 
 	projectNames = td["gh_project_name"]
 	projectNames = projectNames.drop_duplicates()
-
+	projectNames = projectNames.sort_values()
 
 	#dfColumns = list(td.columns.values)
 	dfColumns = list()
 	headers = [
 		"row",
+		"gh_project_name",
 		"job_commit",
 		"comment_commit",
 		"gh_pull_req_num",
@@ -113,7 +117,13 @@ if __name__ == "__main__":
 	]
 	dfColumns.extend(headers)
 
- 	outData = pandas.DataFrame(columns=dfColumns)
+	# We check if there is already a file partially created from a previous run
+	outData	= None
+	fromPartialFile = os.path.isfile(partialOutputfile)
+	if fromPartialFile:
+		outData = pandas.read_csv(partialOutputfile)
+	else:
+ 		outData = pandas.DataFrame(columns=dfColumns)
 
   	#DEBUG
   	#======
@@ -121,84 +131,103 @@ if __name__ == "__main__":
   	# projectNames = albacore["gh_project_name"]
   	# projectNames = projectNames.drop_duplicates()
 	#======
+	currentPrj =""
+	completed=False
+	try:
+	  	print "Fetching comments..."
+	  	index=0
+	  	for label, prj in projectNames.iteritems():
 
-  	print "Fetching comments..."
-  	index=0
-  	for label, prj in projectNames.iteritems():
-  		print "Progress : %s/%s : fetching comments for %s"%(index,len(projectNames),prj)
-
-  		prjData = td[td.gh_project_name == prj]
-  		
-  		# We first get all repo comments
-  		nbMatched=0
-		repoComments = util.fetch_comments(util.repoStr, prj)
-		print "\t%s repo comments found"%(len(repoComments)),
-		if repoComments:
-			for repoComment in repoComments:
-				commitId = repoComment["commit_id"]
-
-				# We try to find an associated commit id in the Travis dataset
-				jobs = util.find_build_jobs_by_commit(prjData, commitId)
-				if not jobs.empty:
-					for label, job in jobs.iterrows():
-						outData.loc[len(outData)] = build_comment_data(repoComment, job, commitId,util.repoStr);
-					nbMatched=nbMatched+1
-			print "\t%s matched"%(nbMatched),
-		print ""
-
-		# We then get all pull request comments
-		nbMatched=0
-		pullRequestComments = util.fetch_comments(util.prStr, prj)
-		print "\t%s pull request comments found"%(len(pullRequestComments)),
-		if pullRequestComments:
-			for prComment in pullRequestComments:
-
-				#There are two fields associated with commit 
-				commitId = prComment["commit_id"]
-				originalCommitId = prComment["original_commit_id"]
-
-				# We try to find an associated commit id in the Travis dataset for both
-				jobs = util.find_build_jobs_by_commit(prjData, commitId)
-				if jobs.empty:
-					jobs = util.find_build_jobs_by_commit(prjData, originalCommitId)
-
-				if not jobs.empty:
-					for label, job in jobs.iterrows():
-						outData.loc[len(outData)] = build_comment_data(prComment, job, commitId, util.prStr);				
-					nbMatched=nbMatched+1
-			print "\t%s matched"%(nbMatched),
-		print ""	
-
-		# Finally, we retrieve issue comments
-		# I make the assumption that on GitHub, the issue number corresponds to the pull 
-		# request number, if the association exists. This seems to hold true but this is not confirmed.
-		nbMatched=0
-		issueComments = util.fetch_comments(util.issueStr, prj)
-		print "\t%s issue comments found"%(len(issueComments)),
-		if issueComments:
-			for issueComment in issueComments:
-
-				# We can find the pull request number in the url...
-				urlParts = issueComment["issue_url"].split("/")
-				prNum = int(urlParts[len(urlParts)-1])
-
-				jobs = prjData[prjData.gh_pull_req_num == prNum]
-				if not jobs.empty:
-					for label, job in jobs.iterrows():
-						outData.loc[len(outData)] = build_comment_data(issueComment, job, "", util.issueStr);
-					nbMatched=nbMatched+1
-						
-			print "\t%s matched"%(nbMatched),
-		print ""
-
-		index=index+1
+	  		o = outData[outData["gh_project_name"] == prj]
+	  		if not o.empty:
+	  			# We already fetched this project's comments during a previous run.
+				print "Progress : %s/%s : Skipping %s. Data already fetched."%(index,len(projectNames),prj)	  			
+				index = index+1
 
 
-	print "Fetching completed!"
-	#ipdb.set_trace()
+	  		print "Progress : %s/%s : fetching comments for %s"%(index,len(projectNames),prj)
+	  		currentPrj = prj	
+	  		prjData = td[td.gh_project_name == prj]
+	  		
+	  		# We first get all repo comments
+	  		nbMatched=0
+			repoComments = util.fetch_comments(util.repoStr, prj)
+			print "\t%s repo comments found"%(len(repoComments)),
+			if repoComments:
+				for repoComment in repoComments:
+					commitId = repoComment["commit_id"]
 
-	# We can finally write the data to a csv file
-	outData.to_csv(outputfile, sep=';', encoding='utf-8', escapechar='\\')
+					# We try to find an associated commit id in the Travis dataset
+					jobs = util.find_build_jobs_by_commit(prjData, commitId)
+					if not jobs.empty:
+						for label, job in jobs.iterrows():
+							outData.loc[len(outData)] = build_comment_data(repoComment, job, commitId,util.repoStr);
+						nbMatched=nbMatched+1
+				print "\t%s matched"%(nbMatched),
+			print ""
+
+			# We then get all pull request comments
+			nbMatched=0
+			pullRequestComments = util.fetch_comments(util.prStr, prj)
+			print "\t%s pull request comments found"%(len(pullRequestComments)),
+			if pullRequestComments:
+				for prComment in pullRequestComments:
+
+					#There are two fields associated with commit 
+					commitId = prComment["commit_id"]
+					originalCommitId = prComment["original_commit_id"]
+
+					# We try to find an associated commit id in the Travis dataset for both
+					jobs = util.find_build_jobs_by_commit(prjData, commitId)
+					if jobs.empty:
+						jobs = util.find_build_jobs_by_commit(prjData, originalCommitId)
+
+					if not jobs.empty:
+						for label, job in jobs.iterrows():
+							outData.loc[len(outData)] = build_comment_data(prComment, job, commitId, util.prStr);				
+						nbMatched=nbMatched+1
+				print "\t%s matched"%(nbMatched),
+			print ""	
+
+			# Finally, we retrieve issue comments
+			# I make the assumption that on GitHub, the issue number corresponds to the pull 
+			# request number, if the association exists. This seems to hold true but this is not confirmed.
+			nbMatched=0
+			issueComments = util.fetch_comments(util.issueStr, prj)
+			print "\t%s issue comments found"%(len(issueComments)),
+			if issueComments:
+				for issueComment in issueComments:
+
+					# We can find the pull request number in the url...
+					urlParts = issueComment["issue_url"].split("/")
+					prNum = int(urlParts[len(urlParts)-1])
+
+					jobs = prjData[prjData.gh_pull_req_num == prNum]
+					if not jobs.empty:
+						for label, job in jobs.iterrows():
+							outData.loc[len(outData)] = build_comment_data(issueComment, job, "", util.issueStr);
+						nbMatched=nbMatched+1
+							
+				print "\t%s matched"%(nbMatched),
+			print ""
+
+			lastCompletedPrj = prj
+			index = index+1
+
+
+		print "Fetching completed!"
+		completed = True
+
+		outData.to_csv(outputfile, sep=';', encoding='utf-8', escapechar='\\')
+		os.remove(partialOutputfile)
+
+	except:
+		# If an error occured, we dump the recovered data so we don't have to fetch it again
+		if not completed and currentPrj:
+			# But first, we remove the data concerning the project that didn't complete
+			outData = outData[outData["gh_project_name"] != currentPrj]
+			with open(partialOutputfile, 'a') as f:
+				outData.to_csv(f, header=False, sep=';', encoding='utf-8', escapechar='\\')
 	
 
 
